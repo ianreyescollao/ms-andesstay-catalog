f89b08c05fc50270199caa501dabda0791be346a
ianreyescollao <ia.reyes@duocuc.cl>
2026-09-15 14:06:34 -0300

    cambios de intragcion

---
+++ src/main/java/cl/mscatalogs/MsCatalogsApplication.java
@@ -0,0 +1,13 @@
+package cl.mscatalogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCatalogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCatalogsApplication.class, args);
+    }
+
+}
